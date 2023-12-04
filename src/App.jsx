e810ebcd900038d57eec058538496a0dbcd3b67f
rezha4 <rezha.bahari@gmail.com>
2023-12-04 13:48:52 +0700

    refactor to React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello From App</p>
+    </>
+  );
+}
+
+export default App;
